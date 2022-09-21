1ccd9b45cf55fadde39b1676335ab94d0fc7eaae
Chris Tomshack <ctomshack15@gmail.com>
2022-09-21 16:52:42 -0400

    homepage - hero & blog section nearly complete

---
+++ components/Socials.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from '../styles/Socials.module.css';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
+import Link from 'next/link';
+
+const Socials = () => {
+  return (
+    <div className={styles.container}>
+        <Link href='https://www.linkedin.com/in/christomshack/'>
+        <FaLinkedinIn size={18} className={styles.icon}/>
+        </Link>
+        <Link href='https://github.com/Ctomshack'>
+        <FaGithub size={18} className={styles.icon}/>
+        </Link>
+        <Link href='https://www.instagram.com/christomshack_/'>
+        <FaInstagram size={18} className={styles.icon}/>
+        </Link>
+        <Link href='https://twitter.com/ChrisTomshack_'>
+        <FaTwitter size={18} className={styles.icon}/>
+        </Link>
+    
+    </div>
+  )
+}
+
+export default Socials
