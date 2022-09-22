955b16c8ea35f470f4bed13bff1a84197cc607b9
Chris Tomshack <ctomshack15@gmail.com>
2022-09-22 16:45:52 -0400

    refactord blog section with a new folder /components/blogArticls to clean the code into their own components

--- components/BlogSection.js
+++ components/BlogSection.js
@@ -4,46 +4,20 @@ import Image from "next/image";
 import Socials from "../components/Socials";
 import { FaLongArrowAltRight } from 'react-icons/fa';
 import Link from "next/link";
+import FirstArticle from './blogArticles/FirstArticle';
+import SecondArticle from './blogArticles/SecondArticle';
+import ThirdArticle from './blogArticles/ThirdArticle';
+import FourthArticle from './blogArticles/FourthArticle';
 
 const BlogSection = () => {
   return (
     <div className={styles.blogContainer}>
             <div className={styles.blogSectionTitle}>Blog</div>
             <div className={styles.blogArticles}>
-              <div className={styles.blogArticle} style={{borderTop: '2px solid gold'}}>
-                <div className={styles.articleCategory} style={{color: 'gold'}}>FITNESS</div>
-                <div className={styles.articleTitle}>My First Triathlon - IRONMAN Cozumel</div>
-                <div className={styles.articleExcerpt}>{`"If you have to prove something to someone, make that someone yourself"`}</div>
-                <div className={styles.articleExcerpt}>{`RACE MORNING - I slept through the night until...`}</div>
-                <Link href='/article/ironman-cozumel-race-report'>
-                   <button className={styles.btnPrimary} style={{margin: '0 2rem 1rem 2rem'}}>Read more<i className="bi bi-arrow-right-short" style={{marginLeft: '.25rem'}}></i></button>
-                </Link>
-              </div>
-              <div className={styles.blogArticle}>
-                <div className={styles.articleCategory}>SELF IMPROVEMENT</div>
-                <div className={styles.articleTitle}>Goal Setting - How To Set Goals And Why You Should</div>
-                <div className={styles.articleExcerpt}>{"In 1979, Harvard conducted a survey wherein they asked the graduating MBA class: “Have you set clear, written goals for..."}</div>
-                <Link href='/article/goal-setting-how-to-set-goal-and-why-you-should'>
-                   <button className={styles.btnPrimary} style={{margin: '0 2rem 1rem 2rem'}}>Read more<i className="bi bi-arrow-right-short" style={{marginLeft: '.25rem'}}></i></button>
-                </Link>
-              </div>
-              <div className={styles.blogArticle} style={{borderTop: '2px solid #ff6489'}}>
-                <div className={styles.articleCategory} style={{color: '#ff6489'}}>PRODUCTIVITY</div>
-                <div className={styles.articleTitle}>How I Increased My Reading Speed By 300% In 20 Minutes</div>
-                <div className={styles.articleExcerpt}>{`"Not all readers are leaders, but all leaders are readers.” - Harry S. Truman`}</div>
-                <div className={styles.articleExcerpt}>{`You have probably heard this...`}</div>
-                <Link href='/article/how-i-increased-my-reading-speed-by-300-percent-in-20-minutes'>
-                   <button className={styles.btnPrimary} style={{margin: '0 2rem 1rem 2rem'}}>Read more<i className="bi bi-arrow-right-short" style={{marginLeft: '.25rem'}}></i></button>
-                </Link>
-              </div>
-              <div className={styles.blogArticle}>
-                <div className={styles.articleCategory}>SELF IMPROVEMENT</div>
-                <div className={styles.articleTitle}>{`The Spending Problem You Didn't Know You Have - The Deridot Effect`}</div>
-                <div className={styles.articleExcerpt}>{`Picture This - You’ve been working out for a while and your friend is constantly pressuring you to come to a crossfit class at their gym. After all, your friend has a...`}</div>
-                <Link href='/article/the-spending-problem-you-did-not-know-you-had'>
-                   <button className={styles.btnPrimary} style={{margin: '0 2rem 1rem 2rem'}}>Read more<i className="bi bi-arrow-right-short" style={{marginLeft: '.25rem'}}></i></button>
-                </Link>
-              </div>
+              <FirstArticle />
+              <SecondArticle />
+              <ThirdArticle />
+              <FourthArticle />
             </div>
             <div>
             <Link href='/blog'>
